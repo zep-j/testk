e3ba154a6addbbfe9b843ae1df4b3d464f409141
na <na@na>
2018-02-27 19:50:24 -0500

    initial check-in

---
+++ testk/src/testk.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : testk.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/*! \brief Brief description.
+ *         Brief description continued.
+ *
+ *  Detailed description starts here.
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+class dum{
+public:
+	int i;
+};
+
+class d2 : public dum {
+public:
+	int k;
+};
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
